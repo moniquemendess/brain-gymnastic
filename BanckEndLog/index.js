90cd19938d66e01875872518a58b86e8922031e1
Monique Mendes <moniiquemendes96@gmail.com>
2024-03-19 19:55:31 +0100

    instación de cors

--- BanckEndLog/index.js
+++ BanckEndLog/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const { connect } = require("./src/utils/db");
+const cors = require("cors");
 
 // Creamos el servidor web.
 const app = express();
@@ -10,6 +11,9 @@ dotenv.config();
 
 // Creamos la conexión con la BD (base de datos)
 connect();
+
+// Permite solicitudes desde cualquier origen
+app.use(cors());
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () =>
