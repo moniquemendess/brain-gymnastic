62847d70d5b224f4b3af9b9b3fde46e5f5300bc5
InesMedem <inesmedem93@gmail.com>
2024-03-21 12:00:15 +0100

    modelo comentarios

---
+++ BackEndLog/src/api/models/Comment.model.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const CommentSchema = new mongoose.Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+    },
+
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    recipientLogic: { type: mongoose.Schema.Types.ObjectId, ref: "Logic" },
+
+    likes: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+const Comment = mongoose.model("Comment", CommentSchema);
+
+module.exports = Comment;
