01b0048466414376ec1b5b98e99e76b754891674
Monique Mendes <moniiquemendes96@gmail.com>
2024-03-21 10:51:54 +0100

    En progreso modelo de datos user

--- BackEndLog/src/api/models/User.model.js
+++ BackEndLog/src/api/models/User.model.js
@@ -1,18 +1,36 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 const UserSchema = new mongoose.Schema({
-  name: {
+  userName: {
     type: String,
     required: true,
     unique: true,
     trim: true,
   },
-  nickName: {
-    type: String,
-    trim: true,
-  },
+
   email: {
     type: String,
+    required: true,
+    validator: [validator.isEmail, "Email not valid"],
+    unique: true,
     trim: true,
   },
+  rol: {
+    type: String,
+    enum: ["admin", "user"],
+    default: "user",
+  },
+  image: {
+    type: String,
+  },
+  password: {
+    type: String,
+    required: true,
+    validate: [validator.isStrongPassword],
+  },
 });
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports = User;
