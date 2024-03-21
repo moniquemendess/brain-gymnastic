9ef5eae4613e52f3cdb7462e08d79e147b986e36
Monique Mendes <moniiquemendes96@gmail.com>
2024-03-21 11:45:17 +0100

    model de user

--- BackEndLog/src/api/models/User.model.js
+++ BackEndLog/src/api/models/User.model.js
@@ -1,35 +1,49 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
 
-const UserSchema = new mongoose.Schema({
-  userName: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
-  },
+const UserSchema = new mongoose.Schema(
+  {
+    userName: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
 
-  email: {
-    type: String,
-    required: true,
-    validator: [validator.isEmail, "Email not valid"],
-    unique: true,
-    trim: true,
+    email: {
+      type: String,
+      required: true,
+      validator: [validator.isEmail, "Email not valid"],
+      unique: true,
+      trim: true,
+    },
+    rol: {
+      type: String,
+      enum: ["admin", "user"],
+      default: "user",
+    },
+    image: {
+      type: String,
+    },
+    gender: {
+      type: String,
+      enum: ["Man", "Women", "Others"],
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      validate: [validator.isStrongPassword],
+    },
+    userComments: [{ type: mongoose.Schema.Types.ObjectId, ref: "Comment" }],
+    userResponse: [{ type: mongoose.Schema.Types.ObjectId, ref: "Results" }],
+    userLikedComments: [
+      { type: mongoose.Schema.Types.ObjectId, ref: "Comment" },
+    ],
+    userLikedLogic: [{ type: mongoose.Schema.Types.ObjectId, ref: "Logic" }],
   },
-  rol: {
-    type: String,
-    enum: ["admin", "user"],
-    default: "user",
-  },
-  image: {
-    type: String,
-  },
-  password: {
-    type: String,
-    required: true,
-    validate: [validator.isStrongPassword],
-  },
-});
+  { timestamps: true }
+);
 
 const User = mongoose.model("User", UserSchema);
 
