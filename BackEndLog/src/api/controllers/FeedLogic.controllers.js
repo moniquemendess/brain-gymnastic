3000f97a7c894d9cedb152111994f61b1bc1f54c
Monique Mendes <155989994+moniquemendess@users.noreply.github.com>
2024-03-26 09:30:44 +0100

    controler de feedlogic (#5)

--- BackEndLog/src/api/controllers/FeedLogic.controllers.js
+++ BackEndLog/src/api/controllers/FeedLogic.controllers.js
@@ -11,27 +11,30 @@ const createLogic = async (req, res) => {
     // Sincronizar índices de la logica
     await FeedLogic.syncIndexes();
 
-    // Creando el objeto logica
+    // Criando o objeto lógica
     const logicData = {
       content: req.body.content,
       logicFeedOwner: req.user._id,
     };
 
-    // Criar nueva instância de logica
+    // Criar nova instância de lógica
     const newLogic = new FeedLogic(logicData);
 
-    // Salvar la  logica en el banco de dados
+    // Salvar a lógica no banco de dados
     const savedLogic = await newLogic.save();
 
-    // Atualizar o usuário com o ID do logic criado
+    // Atualizar o usuário com o ID da lógica criada
     await User.findByIdAndUpdate(req.user._id, {
       $push: { logicFeedOwner: newLogic._id },
     });
+
     console.log("Usuário atualizado com o ID da nova lógica.");
-    return res.status(200).json("El usuario ha creado la logica");
+
+    // Retornar os dados da lógica criada na resposta
+    return res.status(200).json(savedLogic);
   } catch (error) {
     console.error("Erro ao criar a lógica:", error);
-    res.status(400).json({
+    return res.status(400).json({
       error: "Error al crear la logica",
       message: error.message,
     });
