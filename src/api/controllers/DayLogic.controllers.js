f33e878a8b85d2a53f8e16b24be846b155d08874
Monique Mendes <moniiquemendes96@gmail.com>
2024-04-17 17:06:42 +0200

    controlador de daylogic

--- src/api/controllers/DayLogic.controllers.js
+++ src/api/controllers/DayLogic.controllers.js
@@ -1,89 +1,43 @@
-// //----------------------------------------(Importaciones)----------------------------------------------------------------------
-// const fs = require("fs");
-// const path = require("path");
-// const data = require("../../data/enigmas.json");
+//----------------------------------(Importaciones)------------------------------------------------------------------------
 
-// //----------------------------------------(Create DayLogic)----------------------------------------------------------------------
+const cron = require("node-cron");
+const Enigma = require("../models/Enigma.model");
 
-// // Função para criar um enigma aleatório
-// const createDayLogic = async () => {
-//   try {
-//     // Escolher um enigma aleatório do arquivo data/enigmas.json
-//     const randomRiddle = data[Math.floor(Math.random() * data.length)];
+//----------------------------------(Get Random Enigma(aleatorio))------------------------------------------------------------------------
 
-//     // Aqui você iria salvar o enigma no banco de dados MongoDB
-//     console.log("Enigma criado:", randomRiddle);
+let dailyEnigma = null;
 
-//     // Retornar o enigma criado
-//     return randomRiddle;
-//   } catch (error) {
-//     console.error("Erro ao criar o enigma:", error);
-//     throw new Error("Erro ao criar o enigma");
-//   }
-// };
+const getRandomEnigma = async () => {
+  try {
+    const count = await Enigma.countDocuments();
+    const randomIndex = Math.floor(Math.random() * count);
+    const randomEnigma = await Enigma.findOne().skip(randomIndex);
+    return randomEnigma;
+  } catch (error) {
+    throw new Error("Erro ao obter enigma aleatório");
+  }
+};
 
-// // Função para programar a criação do próximo enigma após um dia
-// const nextLogic = async () => {
-//   try {
-//     // Tempo de espera de 1 dia em milissegundos
-//     const day = 24 * 60 * 60 * 1000;
+//----------------------------------(Update del Enigma )------------------------------------------------------------------------
 
-//     // Criar um temporizador para aguardar um dia
-//     setTimeout(async () => {
-//       try {
-//         const newLogic = await createDayLogic();
-//         // Chamando a próxima lógica após o tempo especificado
-//         await nextLogic();
-//       } catch (error) {
-//         console.error("Erro ao criar próximo enigma:", error);
-//       }
-//     }, day);
-//   } catch (error) {
-//     console.error("Erro ao programar próximo enigma:", error);
-//     throw new Error("Erro ao programar próximo enigma");
-//   }
-// };
+const updateDailyEnigma = async () => {
+  try {
+    dailyEnigma = await getRandomEnigma();
+  } catch (error) {
+    console.error("Erro ao atualizar enigma do dia:", error);
+  }
+};
+//----------------------------------(Configuracion del horario del enigma )------------------------------------------------------------------------
 
-// // Iniciar o processo de criação de enigmas
-// createDayLogic()
-//   .then(() => {
-//     // Programar a criação do próximo enigma após um dia
-//     nextLogic();
-//   })
-//   .catch((error) => {
-//     console.error("Erro ao iniciar a criação de enigmas:", error);
-//   });
+// Agendando la atualizacion del enigma diariamente a las 8h de la mañana
+cron.schedule("0 8 * * *", async () => {
+  await updateDailyEnigma();
+});
 
-// const createAndScheduleEnigmaCreation = async () => {
-//   try {
-//     // Criar um enigma
-//     const newEnigma = await createDayLogic();
-//     console.log("Enigma criado:", newEnigma);
+const getDailyEnigma = () => {
+  return dailyEnigma;
+};
 
-//     // Programar a criação do próximo enigma após um dia
-//     await nextLogic();
+//----------------------------------(Exportaciones)------------------------------------------------------------------------
 
-//     console.log("Próximo enigma programado com sucesso.");
-//   } catch (error) {
-//     console.error("Erro ao criar ou programar enigma:", error);
-//     throw new Error("Erro ao criar ou programar enigma");
-//   }
-// };
-
-// // Chamar a função para criar um enigma e programar a próxima criação
-// createAndScheduleEnigmaCreation()
-//   .then(() => {
-//     console.log(
-//       "Processo de criação de enigmas e programação do próximo enigma iniciado com sucesso!"
-//     );
-//   })
-//   .catch((error) => {
-//     console.error(
-//       "Erro ao iniciar o processo de criação e programação de enigmas:",
-//       error
-//     );
-//   });
-
-// //----------------------------------------(Exportaciones)----------------------------------------------------------------------
-
-// module.exports = { createAndScheduleEnigmaCreation };
+module.exports = { getDailyEnigma };
--- src/api/routes/DayLogic.routes.js
+++ src/api/routes/DayLogic.routes.js
@@ -1,7 +1,6 @@
 //------------------------------------(Importaciones)------------------------------------------------------------------
 
-// const { data } = require("../../data/enigmas.json");
-// const { createDayLogic } = require("../controllers/DayLogic.controllers");
+const { getDailyEnigma } = require("../controllers/DayLogic.controllers");
 
 //----------------------------(Configuración de la Rutas con Express)----------------------------------------------------
 
@@ -11,12 +10,7 @@ const DayLogicRoutes = require("express").Router();
 
 //localhost:8080/api/v1/dayLogic
 
-// DayLogicRoutes.get("/enigmas", (req, res) => {
-//   let riddleObj = data[Math.floor(Math.random() * data.length)];
-//   res.send(riddleObj);
-// });
-
-// DayLogicRoutes.post("/create", createAndScheduleEnigmaCreation);
+DayLogicRoutes.get("/daily", getDailyEnigma);
 
 //---------------------------------------(Rutas Privadas)--------------------------------------------------------------
 
